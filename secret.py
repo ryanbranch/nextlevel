e42aeb334905951feb77fcab7991a1f290959497
Ryan Branch <rjbrjb@umich.edu>
2021-06-25 23:19:34 -0400

    Create secret.py
    NOTE: The version of secret.py contained in this commit contains no confidential information.
    
    Created secret.py which will serve as a single file for storing all confidential information in one central location. Currently this encompasses the Discord authtoken and AWS S3 bucket name, but will also soon include an Etherscan authtoken.

---
+++ secret.py
@@ -0,0 +1,11 @@
+# DEFAULT CONTENT for secret.py
+# WARNING: This is the default version of this file, which contains no confidential information
+#          secret.py is included in the .gitignore in order to prevent accidental publication of confidential data
+#          Please be careful not to accidentally share your secret.py file in any public or insecure location!
+
+# AUTHENTICATION TOKENS
+AUTH_TOKEN_DISCORD = ""
+AUTH_TOKEN_ETHERSCAN = ""
+
+# AWS S3 BUCKET STRING
+AWS_S3_BUCKET = ""
